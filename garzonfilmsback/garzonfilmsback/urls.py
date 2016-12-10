e029412aa48510329f731536c10ed760df81c1b3
Jesus A. G <xtornasol512@gmail.com>
2016-12-10 03:20:39 -0600

    agregando statics haciendo migraciones

--- garzonfilmsback/garzonfilmsback/urls.py
+++ garzonfilmsback/garzonfilmsback/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ] 
 
-if DEBUG:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
